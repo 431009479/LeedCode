4d5de1b09005188ccd7036f7bfc0731488d15ce9
431009479 <431009479@qq.com>
2020-02-27 19:23:55 +0800

    Leed_20

---
+++ Leed_20.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: Leed_20.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月27日 星期四 19时23分23秒
+ ************************************************************************/
+bool isValid(char * s){
+    int len = strlen(s);
+    char *stack = (char *)malloc(sizeof(char) * len);
+    int top = -1,  flag = 1;
+    while(s[0]) {
+        switch (s[0]){
+        case '(' :
+        case '{' :
+        case '[' :   stack[++top] = s[0]; break;
+        case ')' : flag = (top != -1 && stack[top--] == '(') ;break;
+        case ']' : flag = (top != -1 && stack[top--] == '[') ;break;
+        case '}' : flag = (top != -1 && stack[top--] == '{') ;break;
+        }
+        if(!flag) break;
+        s++;
+    }
+    free(stack);
+    return (flag && top == -1);
+}
