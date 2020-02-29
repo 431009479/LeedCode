4126d9283663d7c9d0a6dad2f8c5a780f6f8c4d2
431009479 <431009479@qq.com>
2020-02-29 16:46:50 +0800

    Leed_107

---
+++ Leed_107.cpp
@@ -0,0 +1,43 @@
+/*************************************************************************
+	> File Name: Leed_107.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月29日 星期六 16时46分28秒
+ ************************************************************************/
+
+int getDepth(struct TreeNode *root){
+    if(root == NULL) return 0;
+    int l = getDepth(root->left),  r = getDepth(root->right);
+    return (l > r ? l : r) + 1;
+
+}
+void getCnt(struct TreeNode *root, int k, int * cnt){
+    if(root == NULL) return ;
+    cnt[k]++;
+    getCnt(root->left, k - 1, cnt);
+    getCnt(root->right, k -1, cnt);
+    return ;
+}
+
+void getResult(struct TreeNode *root, int k, int *cnt, int **ret){
+    if(root == NULL) return ;
+    ret[k][cnt[k]++] = root->val;
+    getResult(root->left, k - 1, cnt, ret);
+    getResult(root->right, k - 1, cnt, ret);
+    return ;
+}
+
+int **levelOrderBottom(struct TreeNode* root, int* returnSize, int** returnColumnSizes){
+        int n = getDepth(root);
+        int **ret = (int **)malloc(sizeof(int *) * n);
+        int *cnt = (int *)calloc(n, sizeof(int));
+        getCnt(root,  n - 1, cnt);
+        for(int i = 0; i < n; i++){
+            ret[i] = (int *)malloc(sizeof(int) * cnt[i]);
+            cnt[i] = 0;
+        }
+        getResult(root, n - 1, cnt, ret);
+        *returnSize = n;
+        *returnColumnSizes = cnt;
+        return ret;
+}
